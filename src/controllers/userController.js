d108bab88b17d96de56dfd70eaa6e8965178fc42
Daniel Meireles <daniel@inovadora.com.br>
2022-01-18 19:18:14 -0300

    Ajustando rotas de users.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,9 +6,6 @@ module.exports = {
          *
             #swagger.tags = ['User']
             #swagger.description = 'Endpoint que lista todos os usuários cadastrados.'
-            #swagger.responses[200] = {},
-                description: "Retorna um array de usuários ou vazio."} 
-            #swagger.responses[500] = { description: "Server Failure." }
         */
         try {
             const users = await getAllUsers()
@@ -21,16 +18,7 @@ module.exports = {
         /**
          *
             #swagger.tags = ['User']
-            #swagger.description = 'Endpoint para registrar um novo usuario.'
-            #swagger.parameters['obj'] = {
-                in: 'body',
-                description: 'Parametros necessarios para criar um novo usuário.',
-                required: true,
-                schema: { $ref: "#/definitions/User" }
-            }
-            #swagger.responses[200] = { 
-                schema: { "$ref": "#/definitions/User" },
-                description: "Usuario registrado com sucesso." } 
+            #swagger.description = 'Endpoint que mostra os dados de um usuário cadastrado.'
         */
         try {
             const { userID } = req.params
@@ -47,15 +35,6 @@ module.exports = {
          *
             #swagger.tags = ['User']
             #swagger.description = 'Endpoint para registrar um novo usuario.'
-            #swagger.parameters['obj'] = {
-                in: 'body',
-                description: 'Parametros necessarios para criar um novo usuário.',
-                required: true,
-                schema: { $ref: "#/definitions/User" }
-            }
-            #swagger.responses[200] = { 
-                schema: { "$ref": "#/definitions/User" },
-                description: "Usuario registrado com sucesso." } 
         */
         try {
             const { name, email } = req.body
@@ -72,16 +51,7 @@ module.exports = {
         /**
          *
             #swagger.tags = ['User']
-            #swagger.description = 'Endpoint para registrar um novo usuario.'
-            #swagger.parameters['obj'] = {
-                in: 'body',
-                description: 'Parametros necessarios para criar um novo usuário.',
-                required: true,
-                schema: { $ref: "#/definitions/User" }
-            }
-            #swagger.responses[200] = { 
-                schema: { "$ref": "#/definitions/User" },
-                description: "Usuario registrado com sucesso." } 
+            #swagger.description = 'Endpoint para atualizar um usuario já cadastrado.'
         */
         try {
             const { userID } = req.params
@@ -94,34 +64,6 @@ module.exports = {
             res.status(200).json({ message: "Sucesso", user: userUpdated })
         } catch (e) {
             res.status(404).json({ message: e.message })
-        }
-    },
-    singin: async (req, res) => {
-        /**
-         *
-            #swagger.tags = ['User']
-            #swagger.description = 'Endpoint para registrar um novo usuario.'
-            #swagger.parameters['obj'] = {
-                in: 'body',
-                description: 'Parametros necessarios para criar um novo usuário.',
-                required: true,
-                schema: { $ref: "#/definitions/User" }
-            }
-            #swagger.responses[200] = { 
-                schema: { "$ref": "#/definitions/User" },
-                description: "Usuario registrado com sucesso." } 
-        */
-        try {
-            const user = await handleLogin(req.body)
-            let msg = ''
-            if (!user) {
-                msg = 'Nome e/ou E-mail Invalidos.'
-            } else {
-                msg = 'Login Evetuado com sucesso.'
-            }
-            return res.status(200).json({ message: msg, user})
-        } catch (e) {
-            return res.status(404).json({ message: e.message })
         }
     }
 }
--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -5,7 +5,6 @@ const userController = require('../../controllers/userController')
 usersRoutes.get('/users', userController.index)
 usersRoutes.get('/users/:userID', userController.show)
 usersRoutes.post('/singup', userController.singup)
-usersRoutes.post('/singin', userController.singin)
 usersRoutes.patch('/users/:userID', userController.update)
 
 module.exports = usersRoutes
