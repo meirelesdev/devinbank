b91efc7351b4c1be86d45e4fd7a3b6a61686aa77
Daniel Meireles <daniel@inovadora.com.br>
2022-01-20 20:13:26 -0300

    adicionando rota rais com redirect.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const transactionsRoutes = require('./v1/transactionRoutes')
 const usersRoutes = require('./v1/userRoutes')
 const routes = express.Router()
 
+routes.get('/', (req, res)=>{
+    res.redirect('/api/v1/docs')
+})
 routes.use('/api/v1/', [usersRoutes,transactionsRoutes])
 
 module.exports = routes
